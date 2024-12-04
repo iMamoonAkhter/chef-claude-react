a86043e11f7f60f12add9141eb5cc7e4a32591e9
imamoonakhter <imamoonakhter@gmail.com>
2024-12-04 21:10:59 +0500

    ad

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import chef from "../../assets/chef.png";
+const Header = () => {
+  return (
+    <div>
+        <header>
+            <img src={chef} alt="chef-logo"/>
+            <h1>Chef Claude</h1>
+        </header>
+    </div>
+  )
+}
+
+export default Header
